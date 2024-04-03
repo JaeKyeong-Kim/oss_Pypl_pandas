394288a52a37f589d51be0a832c1f7849a62e266
JaeKyeong-Kim <106914954+JaeKyeong-Kim@users.noreply.github.com>
2024-04-03 09:36:34 +0900

    Add files via upload
    h

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup (
+  name='mypandas2024_JKK',
+  packages=['mypandas']
+  version='0.0.1',
+  description='oss-dev mypandas app 0001',
+  author = 'jkkim',
+  url = '',
+  isntall_requires=['pandas']
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+
+)
